c56fb0479d80f2a5c8b802e723276aac414d9f6d
Perkles <gabriel_felipe_trabalho@hotmail.com>
2023-02-06 18:00:51 -0300

    atualização 1

---
+++ src/ArraysMultidimencionais.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class ArraysMultidimencionais {
+
+    /*Exercício 4:
+     * Gere e imprima uma matriz M 4X4 com valores aleatórios entre 0-9.
+     */
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[][] M = new int[4][4];
+
+
+    }
+}
